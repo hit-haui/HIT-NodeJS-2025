ad292462876b3d574a7439bd9250bab7db5671d6
ductung3008 <ductungltt@gmail.com>
2025-04-03 22:11:04 +0700

    feat: add MongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
+const morgan = require('morgan');
 const express = require('express');
+const httpStatus = require('http-status-codes');
 
+const connectDB = require('./config/db');
 const homeRoutes = require('./routes/home.route');
 const userRoutes = require('./routes/user.route');
 
@@ -8,15 +11,31 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
 app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static('public'));
 
 app.use('/', homeRoutes);
 app.use('/api/v1/users', userRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use('*', (req, res) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    statusCode: httpStatus.NOT_FOUND,
+    message: 'Tài nguyên không tồn tại.',
+    data: {},
+  });
 });
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database:', err);
+  });
