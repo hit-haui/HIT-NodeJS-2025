dbfe4ad44bdf65ef63f7f03443787caaeee891b5
Quang Nguyen <wangnguenlc79@gmail.com>
2025-04-04 00:51:28 +0700

    fix: api features

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,9 +1,10 @@
 const httpStatus = require('http-status-codes');
 const User = require('../../models/user.model');
-
+const bcrypt = require('bcryptjs');
 const getUsers = async (req, res) => {
   try {
     const users = await User.find({});
+    // .select('-password')
     res.status(httpStatus.OK).json({
       statusCode: httpStatus.OK,
       message: 'Lấy danh sách người dùng',
@@ -13,13 +14,19 @@ const getUsers = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Đã xảy ra lỗi',
+      data: {},
+    });
   }
 };
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
     if (!fullName || !email || !password) {
+      console.log(fullName, email, password);
       res.status(httpStatus.BAD_REQUEST).json({
         statusCode: httpStatus.BAD_REQUEST,
         message: 'Vui lòng nhập đủ các trường',
@@ -28,17 +35,23 @@ const createUser = (req, res) => {
       return;
     }
 
-    const isExistEmail = User.findOne({ email: email });
+    const isExistEmail = await User.findOne({ email: email });
     if (isExistEmail) {
-      res.status(httpStatus.BAD_REQUEST).json({
-        statusCode: httpStatus.BAD_REQUEST,
+      res.status(httpStatus.CONFLICTT).json({
+        statusCode: httpStatus.CONFLICTT,
         message: 'Email đã tồn tại',
         data: {},
       });
       return;
     }
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
 
-    const user = User.create(req.body);
+    const user = await User.create({
+      fullName: fullName,
+      email: email,
+      password: hashPassword,
+    });
     res.status(httpStatus.CREATED).json({
       statusCode: httpStatus.CREATED,
       message: 'Tạo người dùng thành công.',
@@ -48,7 +61,44 @@ const createUser = (req, res) => {
     });
   } catch (error) {
     console.log(error);
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Đã xảy ra lỗi',
+      data: {},
+    });
   }
 };
 
-module.exports = { getUsers, createUser };
+const updateUser = async (req, res) => {
+  try {
+    // const user = await User.findById(req.params.id);
+    // Object.assign(user, req.body);
+    // await user.save();
+
+    const userUpdate = req.body;
+    const user = await User.findByIdAndUpdate(req.params.id, userUpdate);
+    if (!user) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        statusCode: httpStatus.NOT_FOUND,
+        message: 'Người dùng không tồn tại',
+        data: {},
+      });
+    }
+
+    res.status(httpStatus.OK).json({
+      statusCode: httpStatus.OK,
+      message: 'Cập nhật người dùng thành công.',
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Đã xảy ra lỗi',
+      data: {},
+    });
+  }
+};
+
+module.exports = { getUsers, createUser, updateUser };
--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -5,8 +5,8 @@ const userController = require('../../controllers/client/user.controller');
 router.get('/', userController.getUsers);
 router.post('/', userController.createUser);
 
+router.put('/:id', userController.updateUser);
 // router.get('/:id', userController.getUserById);
-// router.put('/:id', userController.updateUser);
 // router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
